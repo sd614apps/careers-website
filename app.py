bd92cb6b65b6778271a307bd44f2b5f1b532886d
SD614 Apps <126702384+sd614apps@users.noreply.github.com>
2023-03-09 13:33:10 +0000

    Added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from flask import Flask, render_template
+from database import load_jobs_from_db, load_job_from_db
 
 
 app = Flask(__name__)
-
+company_name = "SD614 Apps"
 
 @app.route("/")
 def hello_sd614apps():
   jobs = load_jobs_from_db()
   return render_template("home.html",
                          jobs=jobs,
-                         company_name="SD614 Apps")
+                         company_name=company_name)
 
 
 @app.route("/api/jobs")
@@ -18,6 +18,18 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jobs
 
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "Not Found", 404
+  
+  return render_template("jobpage.html",
+                        job=job,
+                        company_name=company_name)
+  
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
