56d827c508a7f2a361b6a15f4455c43da64a63da
SD614 Apps <126702384+sd614apps@users.noreply.github.com>
2023-03-11 09:02:42 +0000

    store application in DB

--- app.py
+++ app.py
@@ -1,48 +1,51 @@
 from flask import Flask, render_template, request
-from database import load_jobs_from_db, load_job_from_db
-
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 company_name = "SD614 Apps"
 
+
 @app.route("/")
 def hello_sd614apps():
-  jobs = load_jobs_from_db()
-  return render_template("home.html",
-                         jobs=jobs,
-                         company_name=company_name)
+    jobs = load_jobs_from_db()
+    return render_template("home.html",
+                           jobs=jobs,
+                           company_name=company_name)
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs = load_jobs_from_db()
-  return jobs
+    jobs = load_jobs_from_db()
+    return jobs
 
 
 @app.route("/job/<id>")
 def show_job(id):
-  job = load_job_from_db(id)
-  
-  if not job:
-    return "Not Found", 404
-  
-  return render_template("jobpage.html",
-                        job=job,
-                        company_name=company_name)
+    job = load_job_from_db(id)
+
+    if not job:
+        return "Not Found", 404
+
+    return render_template("jobpage.html",
+                           job=job,
+                           company_name=company_name)
 
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
-  data = request.form
-  job = load_job_from_db(id)
-  # store this in the DB
-  # send an email acknowledgement
-  # display the acknowledgement
-  return render_template("application_submitted.html",
-                        application=data,
-                        job = job,
-                        company_name=company_name)
-  
+    data = request.form
+    job = load_job_from_db(id)
+    # store this in the DB
+    add_application_to_db(id, data)
+
+    # send an email acknowledgement
+
+    # display the acknowledgement
+    return render_template("application_submitted.html",
+                           application=data,
+                           job=job,
+                           company_name=company_name)
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -4,31 +4,52 @@ import os
 db_conn_uri = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(
-  db_conn_uri,
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/certs/ca-certificates.crt"
-    }
-  })
+    db_conn_uri,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/certs/ca-certificates.crt"
+        }
+    })
 
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    results = conn.execute(text("select * from jobs"))
+    with engine.connect() as conn:
+        results = conn.execute(text("select * from jobs"))
 
-    jobs = []
-    for row in results:
-      jobs.append(dict(row._mapping))
-    return jobs
+        jobs = []
+        for row in results:
+            jobs.append(dict(row._mapping))
+        return jobs
 
 
 def load_job_from_db(id):
-  with engine.connect() as conn:
-    query = text(f"SELECT * FROM jobs WHERE id= {id}")
-    res = conn.execute(query)
+    with engine.connect() as conn:
+        query = text(f"SELECT * FROM jobs WHERE id= {id}")
+        res = conn.execute(query)
 
-    if res.rowcount == 0:
-      return None
+        if res.rowcount == 0:
+            return None
 
-    for row in res:
-      return dict(row._mapping)
+        for row in res:
+            return dict(row._mapping)
+
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text(f"INSERT INTO applications ("
+                     f"job_id,"
+                     f" full_name,"
+                     f" email,"
+                     f" linkedin_url,"
+                     f" education,"
+                     f" work_experience,"
+                     f" resume_url) VALUES ("
+                     f"{job_id},"
+                     f" '{data['full_name']}',"
+                     f" '{data['email']}',"
+                     f" '{data['linkedin_url']}',"
+                     f" '{data['education']}',"
+                     f" '{data['work_experience']}',"
+                     f" '{data['resume_url']}')")
+
+        conn.execute(query)
